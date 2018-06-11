ce1632d850b1b5449c3d4c8905edc158ee62df0c
sosotown <bsk.sooo.1022@gmail.com>
2018-06-11 18:39:50 +0900

    Add profile

--- lib/sosotown.rb
+++ lib/sosotown.rb
@@ -1,8 +1,16 @@
 require "sosotown/version"
+require 'date'
 
 module Sosotown
   def self.me
-    puts 'name: sosotown'
-    puts 'age:  23'
+    puts 'name:   sosotown'
+    puts 'age:    ' + Sosotown.years_difference(Date.new(1994,10,22), Date.today).to_s
+    puts 'HP:     http://sohatakeyama.me/'
+    puts 'FB:     https://www.facebook.com/sososotown'
+    puts 'Github: https://github.com/sosotown'
+  end
+
+  def self.years_difference(old_date, new_date)
+    (new_date.to_time - old_date.to_time).to_i / (365 * 24 * 60 * 60)
   end
 end
